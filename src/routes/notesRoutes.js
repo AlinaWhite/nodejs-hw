f1feb026be61a3e14efb7c4a9227df4e323cb40a
Alina White <164808035+AlinaWhite@users.noreply.github.com>
2026-06-11 17:37:22 +0100

    h2 change 2

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -10,9 +10,9 @@ import {
 const router = Router();
 
 router.get('/notes', getAllNotes);
-router.get('/notes:noteId', getNoteById);
-router.post('/', createNote);
-router.delete('/:noteId', deleteNote);
-router.patch('/:noteId', updateNote);
+router.get('/notes/:noteId', getNoteById);
+router.post('/notes', createNote);
+router.delete('/notes/:noteId', deleteNote);
+router.patch('/notes/:noteId', updateNote);
 
 export default router;
